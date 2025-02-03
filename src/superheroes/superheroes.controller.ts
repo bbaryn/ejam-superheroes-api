90cffd4366be32340171ee21a5ad021b28427d03
Bartłomiej Baryń <b.baryn@gmi.pl>
2025-02-03 10:25:24 +0100

    add-post-endpoint

--- src/superheroes/superheroes.controller.ts
+++ src/superheroes/superheroes.controller.ts
@@ -1,13 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { Superhero } from 'src/common/types';
+import { CreateSuperheroDto } from './dto/createSuperhero.dto';
 import { SuperheroesService } from './superheroes.service';
 
 @Controller('superheroes')
 export class SuperheroesController {
   constructor(private readonly superheroesService: SuperheroesService) {}
 
-  @Get('')
+  @Get()
   getSuperheroes(): Promise<Superhero[]> {
     return this.superheroesService.getSuperheroes();
+  }
+
+  @Post()
+  createSuperhero(@Body() body: CreateSuperheroDto): Promise<Superhero> {
+    return this.superheroesService.createSuperhero(body);
   }
 }
--- src/superheroes/superheroes.service.ts
+++ src/superheroes/superheroes.service.ts
@@ -1,10 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { database } from 'src/common/db';
 import { sortByHumility } from 'src/utils/sortByHumility';
+import { CreateSuperheroDto } from './dto/createSuperhero.dto';
 
 @Injectable()
 export class SuperheroesService {
   async getSuperheroes() {
     return sortByHumility(database);
+  }
+
+  async createSuperhero(dto: CreateSuperheroDto) {
+    const newHeroId = database.length + 1;
+
+    const superHero = {
+      id: newHeroId,
+      ...dto,
+    };
+
+    database.push({
+      ...superHero,
+    });
+
+    return superHero;
   }
 }
